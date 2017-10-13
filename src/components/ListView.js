d54e73c01f4ecc2db612a6005e9adbf7ebbf6720
rouzbeh.majlessi <rouzbeh.majlessi@avanade.com>
2017-10-13 16:16:05 -0700

    fix bugs/syntax errors

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,22 +1,22 @@
-import _from 'lodash';
-import React, {Component} from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 
-export default ListView extends Component {
+export default class ListView extends Component {
 
   render() {
     return (
       <ul>
-      {_.map(this.props.rowsIdArray, this.renderRowById.bind(this))}
+        {_.map(this.props.rowsIdArray, this.renderRowById.bind(this))}
       </ul>
     );
   }
 
-renderRowById(rowId) {
-  return (
-    <li key={rowId}>
-    {this.props.renderRow(_.get(this.props.rowsById, rowId))}
-    </li>
-  );
-}
+  renderRowById(rowId) {
+    return (
+      <li key={rowId}>
+        {this.props.renderRow(_.get(this.props.rowsById, rowId))}
+      </li>
+    );
+  }
 
 }
--- src/containers/TopicsScreen.js
+++ src/containers/TopicsScreen.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import '.TopicsScreen.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './TopicsScreen.css';
 import * as topicsActions from '../store/topics/actions';
 import * as topicsSelectors from '../store/topics/reducer';
 import ListView from '../components/ListView';
@@ -12,13 +12,13 @@ class TopicsScreen extends Component {
   }
 
   render() {
-    if(!this.props.rowsById) return this.renderLoading();
+    if (!this.props.rowsById) return this.renderLoading();
     return (
       <div className="TopicsScreen">
-      <ListView
-      rowsIdArray={this.props.rowsIdArray}
-      rowsById={this.props.rowsById}
-      renderRow={this.renderRow} />
+        <ListView
+          rowsIdArray={this.props.rowsIdArray}
+          rowsById={this.props.rowsById}
+          renderRow={this.renderRow} />
       </div>
     );
   }
@@ -28,20 +28,23 @@ class TopicsScreen extends Component {
       <p>Loading...</p>
     );
   }
-  renderRow () {
+
+  renderRow(row) {
     return (
       <div>
-      <h3>{row.title}</h3>
-      <p>{row.description}</p>
+        <h3>{row.title}</h3>
+        <p>{row.description}</p>
       </div>
     )
   }
+
 }
-//which props do we want to inject given the global store state.
+
+// which props do we want to inject, given the global store state?
 function mapStateToProps(state) {
   return {
     rowsById: topicsSelectors.getTopicsByUrl(state),
-    rowsIdArray: topicsSelectors.getTopicsByUrlArray(state)
+    rowsIdArray: topicsSelectors.getTopicsUrlArray(state)
   };
 }
 
